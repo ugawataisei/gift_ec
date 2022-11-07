fd7038f5bbaf02b661be0a089db7b6ae9c34053f
ugawataisei <ugawa.service@gmail.com>
2022-11-07 16:41:19 +0900

    feature/admin - fix:navi logo fix

--- resources/views/layouts/admin-navigation.blade.php
+++ resources/views/layouts/admin-navigation.blade.php
@@ -6,7 +6,7 @@
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('admin.dashboard') }}">
-                        <x-application-logo class="block h-10 w-auto fill-current text-gray-600" />
+                        <x-navi-logo class="block h-10 w-auto fill-current text-gray-600" />
                     </a>
                 </div>
 
