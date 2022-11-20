0555b252d9eeff2fb683364b8479f08a8d3e1a5d
ugawataisei <ugawa.service@gmail.com>
2022-11-20 23:26:19 +0900

    feat: feature/shop * 編集フォームに画像追加

--- resources/views/owner/shop/edit.blade.php
+++ resources/views/owner/shop/edit.blade.php
@@ -43,6 +43,7 @@ $selectIsSelling = [
                         {{ Form::file('image', ["aria-describedby" => "user_avatar_help", "class" => "block w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 cursor-pointer dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"]) }}
                     </div>
                     {{--  todo: アップロード時サムネイル表示 --}}
+                    <img class="max-w-xs h-auto mb-6" src="{{ asset('storage/images/shops/' . $model->file_name) }}" alt="image description">
                     <div class="mb-6">
                         {{ Form::label('is_selling', '販売状況', ['class'=>'block mb-2 text-sm font-medium text-gray-900 dark:text-white']) }}
                         {{ Form::select('is_selling', $selectIsSelling, null, [ 'placeholder'=>'選択してください', "class" => "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"]) }}
