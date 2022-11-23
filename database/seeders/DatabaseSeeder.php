7f41d886c0b61498ccfb5790603e42347782b662
ugawataisei <ugawa.service@gmail.com>
2022-11-23 15:12:49 +0900

    feat: feature/shop * [Image] Seeder追加

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
             OwnerSeeder::class,
             UserSeeder::class,
             ShopSeeder::class,
+            ImageSeeder::class,
         ]);
     }
 }
---
+++ database/seeders/ImageSeeder.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ImageSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('images')->insert([
+            [
+                'owner_id' => 1,
+                'file_name' => 'sample1.jpg',
+                'title' => '商品1',
+            ],
+            [
+                'owner_id' => 1,
+                'file_name' => 'sample2.jpg',
+                'title' => '商品2',
+            ],
+            [
+                'owner_id' => 1,
+                'file_name' => 'sample3.jpg',
+                'title' => '商品3',
+            ],
+            [
+                'owner_id' => 1,
+                'file_name' => 'sample4.jpg',
+                'title' => '商品4',
+            ],
+            [
+                'owner_id' => 1,
+                'file_name' => 'sample5.jpg',
+                'title' => '商品5',
+            ],
+        ]);
+    }
+}
