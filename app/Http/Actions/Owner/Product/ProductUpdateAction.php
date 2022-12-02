c7ae12a33b81a3caf76c6f7952d7cdedd073682a
ugawataisei <ugawa.service@gmail.com>
2022-12-02 20:43:56 +0900

    feat: feature/shop * [Product] 商品更新処理実装

--- app/Http/Actions/Owner/Product/ProductUpdateAction.php
+++ app/Http/Actions/Owner/Product/ProductUpdateAction.php
@@ -3,7 +3,12 @@
 namespace App\Http\Actions\Owner\Product;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Http\Requests\Product\ProductUpdateRequest;
+use App\Models\Product;
+use App\Models\Stock;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
 class ProductUpdateAction extends Controller
 {
@@ -12,9 +17,75 @@ class ProductUpdateAction extends Controller
         $this->middleware('auth:owners');
     }
 
-    public function __invoke(Request $request)
+    public function __invoke(ProductUpdateRequest $request): RedirectResponse
     {
-        dd($request);
+        try {
+            DB::transaction(function () use ($request) {
+                //商品テーブルへのデータ保存
+                $query = Product::query();
+                $query->where('id', $request->get('id'));
+                $model = $query->first();
+
+                if ($model === null) {
+                    return false;
+                }
+
+                $model->update([
+                    'secondary_category_id' => $request->get('secondary_category_id'),
+                    'image_first' => $request->get('image_first'),
+                    'image_second' => $request->get('image_second'),
+                    'image_third' => $request->get('image_third'),
+                    'image_fourth' => $request->get('image_fourth'),
+                    'name' => $request->get('name'),
+                    'information' => $request->get('information'),
+                    'price' => $request->get('price'),
+                    'is_selling' => $request->get('is_selling'),
+                    'sort_order' => $request->get('sort_order') ?? 1,
+                ]);
+
+                //在庫テーブルへのデータ保存
+                $query = Stock::query();
+                $query->where('product_id', $request->get('id'));
+                $model = $query->first();
+
+                if ($model === null) {
+                    return false;
+                }
+
+                //在庫情報が変更されていた場合リダイレクトさせる
+                if ($model->quantity !== (int)$request->get('quantity')) {
+                    return redirect("owner/product/edit/{$request->get('id')}")
+                        ->with([
+                            'status' => 'alert',
+                            'message' => '在庫情報が既に変更されています。変更をご確認ください',
+                        ]);
+                }
+
+                //操作が出庫の場合 -を付与する
+                if ($request->get('type') === 0) {
+                    $quantity = $request->get('quantity') * -1;
+                } else {
+                    $quantity = $request->get('quantity');
+                }
+
+                $model->update([
+                    'type' => $request->get('type'),
+                    'quantity' => $quantity,
+                ]);
+
+                return true;
+            });
+
+        } catch (\Throwable $error) {
+            Log::error($error);
+            throw $error;
+        }
+
+        return redirect("owner/product/edit/{$request->get('id')}")
+            ->with([
+                'status' => 'info',
+                'message' => '商品情報を更新しました',
+            ]);
     }
 }
 
---
+++ app/Http/Requests/Product/ProductUpdateRequest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Requests\Product;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ProductUpdateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'id' => 'required|numeric',
+            'shop_id' => 'required|numeric',
+            'is_selling' => 'required|numeric',
+            'secondary_category_id' => 'required|numeric',
+            'name' => 'nullable|string',
+            'price' => 'nullable|numeric',
+            'information' => 'nullable|string',
+            'image_first' => 'nullable|numeric',
+            'image_second' => 'nullable|numeric',
+            'image_third' => 'nullable|numeric',
+            'image_fourth' => 'nullable|numeric',
+            'type' => 'nullable|numeric',
+            'quantity' => 'nullable|numeric',
+        ];
+    }
+}
+
+
